1cbb88228a7ba07497f24b90111b49ce8e5887a5
Pratik Markad <pratik.markad1@gmail.com>
2025-12-26 13:28:39 +0530

    add more table inside databse with entries

--- db/ems.sql
+++ db/ems.sql
@@ -16,3 +16,31 @@ create table employees (
     updatedat timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 );
 
+create table departments (
+	id int primary key auto_increment,
+    dept_code VARCHAR(20) unique,
+	department varchar(50) not null
+);
+
+-- insert into departments values(
+--     1, "HR_01", "Human Resources",
+--     2, "IT_02", "Information Technology",
+--     3, "FIN_03", "Finance",
+--     4, "SUPPORT_04", "Support"
+-- );
+
+insert into departments (dept_code, department) values
+	( "HR_01", "Human Resources"),
+    ( "IT_02", "Information Technology"),
+    ( "FIN_03", "Finance"),
+    ( "SUPPORT_04", "Support");
+
+
+create table emp_depart (
+	emp_id int,
+    depart_id int,
+    primary key (emp_id, depart_id),
+    foreign key (emp_id) references employees(id),
+	foreign key (emp_id) references departments(id)
+);
+
