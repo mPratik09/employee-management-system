472e84340763ee35f2d4cc5cd2c416ef7fa541ae
Pratik Markad <pratik.markad1@gmail.com>
2026-01-05 20:55:59 +0530

    Add dummy records to dockerized SQL file

--- db/ems.sql
+++ db/ems.sql
@@ -52,4 +52,25 @@ create table emp_depart (
 
 SET SESSION sql_mode = CONCAT(@@sql_mode, ',NO_AUTO_VALUE_ON_ZERO');
 insert into users (id, first_name, last_name, email, password, contact_num, status, role) 
-		values(0, "Nebula", "support", "support@nebula.co.in", "Support@123", "9087654321", "ASSIGNED", "SUPPORT");
+		values(0, "Nebula", "support", "support@nebula.co.in", "{noop}Support@123", "9087654321", "ASSIGNED", "SUPPORT");
+
+insert into users (first_name, last_name, email, password, contact_num, status, role) 
+		values("Aman", "Chandra", "aman@nebula.co.in", "{noop}Aman@123", "9090909001", "PENDING", NULL);
+insert into users (first_name, last_name, email, password, contact_num, status, role) 
+		values("Atharva", "Kolape", "atharva@nebula.co.in", "{noop}Atharva@123", "9090909004", "PENDING", NULL);
+insert into users (first_name, last_name, email, password, contact_num, status, role) 
+		values("Amit", "Bavalekar", "amit@nebula.co.in", "{noop}Amit@123", "9090909004", "PENDING", NULL);
+insert into users (first_name, last_name, email, password, contact_num, status, role) 
+		values("Arjun", "Deshmukh", "arjun@nebula.co.in", "{noop}Arjun@123", "9090909005", "PENDING", NULL);
+insert into users (first_name, last_name, email, password, contact_num, status, role) 
+		values("Akshay", "Bhoite", "akshay@nebula.co.in", "{noop}Akshay@123", "9090909006", "PENDING", NULL);
+
+
+insert into users (first_name, last_name, email, password, contact_num, status, role) 
+		values("Sanket", "Pimplikar", "sanket@nebula.co.in", "{noop}Sanket@123", "9090909011", "PENDING", NULL);
+insert into users (first_name, last_name, email, password, contact_num, status, role) 
+		values("Somnath", "Kumbhar", "somnath@nebula.co.in", "{noop}Somnath@123", "9090909012", "PENDING", NULL);
+insert into users (first_name, last_name, email, password, contact_num, status, role) 
+		values("Sahil", "Dabhade", "sahil@nebula.co.in", "{noop}Sahil@123", "9090909013", "PENDING", NULL);
+insert into users (first_name, last_name, email, password, contact_num, status, role) 
+		values("Sushil", "Dabhade", "sushil@nebula.co.in", "{noop}Sushil@123", "9090909014", "PENDING", NULL);
