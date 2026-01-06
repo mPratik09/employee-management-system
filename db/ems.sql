3c3df29dce04a7e000b239d6b81f2552d0ad406a
Pratik Markad <pratik.markad1@gmail.com>
2026-01-06 14:03:56 +0530

    Refactor .sql: allow entries with id '0' and add IF EXISTS to DROP DATABASE

--- db/ems.sql
+++ db/ems.sql
@@ -1,4 +1,4 @@
-DROP DATABASE employee_department_system;
+DROP DATABASE if exists employee_department_system;
 
 create database employee_department_system;
 
@@ -23,19 +23,13 @@ create table departments (
 	department varchar(50) not null
 );
 
--- insert into departments values(
---     1, "HR_01", "Human Resources",
---     2, "IT_02", "Information Technology",
---     3, "FIN_03", "Finance",
---     4, "SUPPORT_04", "Support"
--- );
-
-insert into departments (dept_code, department) values
-	( "HR_01", "Human Resources"),
-    ( "IT_02", "Information Technology"),
-    ( "FIN_03", "Finance"),
-    ( "SUPPORT_04", "Support");
-
+SET SESSION sql_mode = CONCAT(@@sql_mode, ',NO_AUTO_VALUE_ON_ZERO');
+insert into departments (id, dept_code, department) values
+	(0, "UA_00", "Unassigned"),
+	(1,  "SUPPORT_01", "Support"),
+	(2,  "HR_02", "Human Resources"),
+    (3, "IT_03", "Information Technology"),
+    (4,  "FIN_04", "Finance");
 
 create table emp_depart (
 	emp_id int,
@@ -52,25 +46,14 @@ create table emp_depart (
 
 SET SESSION sql_mode = CONCAT(@@sql_mode, ',NO_AUTO_VALUE_ON_ZERO');
 insert into users (id, first_name, last_name, email, password, contact_num, status, role) 
-		values(0, "Nebula", "support", "support@nebula.co.in", "{noop}Support@123", "9087654321", "ASSIGNED", "SUPPORT");
+		values(0, "Nebula", "support", "support@nebula.co.in", "{noop}Support@123", "9087654321", "ASSIGNED", "SUPPORT"),
+        (1, "Aman", "Chandra", "aman@nebula.co.in", "{noop}Aman@123", "9090909001", "PENDING", NULL),
+        (2, "Atharva", "Kolape", "atharva@nebula.co.in", "{noop}Atharva@123", "9090909004", "PENDING", NULL),
+        (3, "Amit", "Bavalekar", "amit@nebula.co.in", "{noop}Amit@123", "9090909004", "PENDING", NULL),
+        (4, "Arjun", "Deshmukh", "arjun@nebula.co.in", "{noop}Arjun@123", "9090909005", "PENDING", NULL),
+        (5, "Akshay", "Bhoite", "akshay@nebula.co.in", "{noop}Akshay@123", "9090909006", "PENDING", NULL),
 
-insert into users (first_name, last_name, email, password, contact_num, status, role) 
-		values("Aman", "Chandra", "aman@nebula.co.in", "{noop}Aman@123", "9090909001", "PENDING", NULL);
-insert into users (first_name, last_name, email, password, contact_num, status, role) 
-		values("Atharva", "Kolape", "atharva@nebula.co.in", "{noop}Atharva@123", "9090909004", "PENDING", NULL);
-insert into users (first_name, last_name, email, password, contact_num, status, role) 
-		values("Amit", "Bavalekar", "amit@nebula.co.in", "{noop}Amit@123", "9090909004", "PENDING", NULL);
-insert into users (first_name, last_name, email, password, contact_num, status, role) 
-		values("Arjun", "Deshmukh", "arjun@nebula.co.in", "{noop}Arjun@123", "9090909005", "PENDING", NULL);
-insert into users (first_name, last_name, email, password, contact_num, status, role) 
-		values("Akshay", "Bhoite", "akshay@nebula.co.in", "{noop}Akshay@123", "9090909006", "PENDING", NULL);
-
-
-insert into users (first_name, last_name, email, password, contact_num, status, role) 
-		values("Sanket", "Pimplikar", "sanket@nebula.co.in", "{noop}Sanket@123", "9090909011", "PENDING", NULL);
-insert into users (first_name, last_name, email, password, contact_num, status, role) 
-		values("Somnath", "Kumbhar", "somnath@nebula.co.in", "{noop}Somnath@123", "9090909012", "PENDING", NULL);
-insert into users (first_name, last_name, email, password, contact_num, status, role) 
-		values("Sahil", "Dabhade", "sahil@nebula.co.in", "{noop}Sahil@123", "9090909013", "PENDING", NULL);
-insert into users (first_name, last_name, email, password, contact_num, status, role) 
-		values("Sushil", "Dabhade", "sushil@nebula.co.in", "{noop}Sushil@123", "9090909014", "PENDING", NULL);
+        (6, "Sanket", "Pimplikar", "sanket@nebula.co.in", "{noop}Sanket@123", "9090909011", "PENDING", NULL),
+		(7, "Somnath", "Kumbhar", "somnath@nebula.co.in", "{noop}Somnath@123", "9090909012", "PENDING", NULL),
+		(8, "Sahil", "Dabhade", "sahil@nebula.co.in", "{noop}Sahil@123", "9090909013", "PENDING", NULL),
+		(9, "Sushil", "Dabhade", "sushil@nebula.co.in", "{noop}Sushil@123", "9090909014", "PENDING", NULL);
