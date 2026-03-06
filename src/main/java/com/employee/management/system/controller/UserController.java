4b5d31ce99bdee30de58448c775cbe88cea19f92
Pratik Markad <pratik.markad1@gmail.com>
2026-03-06 15:27:31 +0530

    refactor: remove unused method from UserController.java

--- src/main/java/com/employee/management/system/controller/UserController.java
+++ src/main/java/com/employee/management/system/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,12 +63,6 @@ public class UserController
 		}
 
 		return "redirect:/savedUser";
-	}
-
-	@GetMapping("/savedUser")
-	public String savedUser()
-	{
-		return "savedUser";
 	}
 
 	@PostMapping("/makeRequest")
